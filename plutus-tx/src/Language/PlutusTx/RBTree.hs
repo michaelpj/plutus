62a3f50e05db20bff9d1456e2c25c08d02ff40c0
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2019-03-08 11:41:18 +0000

    Ensure polymorphic lets have type signatures, use TExpQ

--- plutus-tx/src/Language/PlutusTx/RBTree.hs
+++ plutus-tx/src/Language/PlutusTx/RBTree.hs
@@ -1,6 +1,5 @@
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE TemplateHaskell #-}
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE DeriveAnyClass #-}
 {-# LANGUAGE DerivingStrategies #-}
 {-# OPTIONS_GHC -Wno-name-shadowing #-}
@@ -43,8 +42,8 @@ module Language.PlutusTx.RBTree (
 where
 
 import           Prelude                      hiding (lookup, all, length, map, foldr)
-import           Language.Haskell.TH          (Q, TExp)
-import           Language.PlutusTx.Prelude    (all, length, intCompare)
+import           Language.Haskell.TH          (TExpQ)
+import qualified Language.PlutusTx.Prelude    as P
 import           Language.PlutusTx.These
 import           Language.PlutusTx.Lift
 import           Codec.Serialise
@@ -71,93 +70,102 @@ makeLift ''RBTree
 -- Straightforward functions
 ------------------------------------------------------------
 
-nil :: Q (TExp (RBTree k v))
+nil :: TExpQ (RBTree k v)
 nil = [|| Leaf ||]
 
-singleton :: Q (TExp (k -> v -> RBTree k v))
+singleton :: TExpQ (k -> v -> RBTree k v)
 singleton = [|| \k v -> Branch B 1 Leaf k v Leaf ||]
 
-blackHeight :: Q (TExp (RBTree k v -> BlackHeight))
+blackHeight :: TExpQ (RBTree k v -> BlackHeight)
 blackHeight = [|| \case { Leaf -> 0 ; Branch _ h _ _ _ _ -> h } ||]
 
-color :: Q (TExp (RBTree k v -> Color))
+color :: TExpQ (RBTree k v -> Color)
 color = [|| \case { Leaf -> B ; Branch c _ _ _ _ _ -> c } ||]
 
-lookup :: Q (TExp (Comparison k -> k -> RBTree k v -> Maybe v))
+lookup :: TExpQ (Comparison k -> k -> RBTree k v -> Maybe v)
 lookup =
-    [|| \comp k ->
-        let go = \case
+    [||
+    let lookup :: forall k v . Comparison k -> k -> RBTree k v -> Maybe v
+        lookup comp k  = go where
+            go :: RBTree k v -> Maybe v
+            go = \case
                 Leaf -> Nothing
                 Branch _ _ l k' v r -> case comp k k' of
                     LT -> go l
                     GT -> go r
                     EQ -> Just v
-        in go
+    in lookup
     ||]
 
-size :: Q (TExp (RBTree k v -> Int))
-size = [|| \t -> $$length $ $$keys t ||]
+size :: TExpQ (RBTree k v -> Int)
+size = [|| \t -> $$(P.length) $ $$keys t ||]
 
-keys :: Q (TExp (RBTree k v -> [k]))
+keys :: TExpQ (RBTree k v -> [k])
 keys = [|| $$foldr (\(k,_) ks -> k:ks) [] ||]
 
-values :: Q (TExp (RBTree k v -> [v]))
+values :: TExpQ (RBTree k v -> [v])
 values = [|| $$foldr (\(_,v) vs -> v:vs) [] ||]
 
-toList :: Q (TExp (RBTree k v -> [(k, v)]))
+toList :: TExpQ (RBTree k v -> [(k, v)])
 toList = [|| $$foldr (\m ms -> m:ms) [] ||]
 
-map :: Q (TExp ((a -> b) -> RBTree k a -> RBTree k b))
+fromList :: TExpQ (Comparison k -> [(k, v)] -> RBTree k v)
+fromList = [|| \comp ms -> $$(P.foldr) (\(k, v) t -> $$insert comp k v t) $$nil ms ||]
+
+map :: TExpQ ((a -> b) -> RBTree k a -> RBTree k b)
 map =
     [||
-        let map f = \case
-                Leaf -> Leaf
-                Branch c h l k v r -> Branch c h (map f l) k (f v) (map f r)
-        in map
+    let map :: (a -> b) -> RBTree k a -> RBTree k b
+        map f = \case
+            Leaf -> Leaf
+            Branch c h l k v r -> Branch c h (map f l) k (f v) (map f r)
+    in map
     ||]
 
-foldr :: Q (TExp (((k, v) -> b -> b) -> b -> RBTree k v -> b))
+foldr :: TExpQ (((k, v) -> b -> b) -> b -> RBTree k v -> b)
 foldr =
     [||
-        let foldr f acc = \case
-                Leaf -> acc
-                Branch _ _ l k v r ->
-                    let
-                        right = foldr f acc r
-                        center = f (k, v) right
-                        left = foldr f center l
-                    in left
-        in foldr
+    let foldr :: ((k, v) -> b -> b) -> b -> RBTree k v -> b
+        foldr f acc = \case
+            Leaf -> acc
+            Branch _ _ l k v r ->
+                let
+                    right = foldr f acc r
+                    center = f (k, v) right
+                    left = foldr f center l
+                in left
+    in foldr
     ||]
 
 ------------------------------------------------------------
 -- Invariants and validity
 ------------------------------------------------------------
 
-isBalanced :: Q (TExp (RBTree k v -> Bool))
+isBalanced :: TExpQ (RBTree k v -> Bool)
 isBalanced = [|| \t -> $$sameBlacksToLeaves t && $$checkChildren t ||]
 
-sameBlacksToLeaves :: Q (TExp (RBTree k v -> Bool))
+sameBlacksToLeaves :: TExpQ (RBTree k v -> Bool)
 sameBlacksToLeaves =
     [|| \t -> case $$blacksToLeaves t of
               [] -> True
-              n:ns -> $$all (n==) ns
+              n:ns -> $$(P.all) (n==) ns
     ||]
 
-blacksToLeaves :: Q (TExp (RBTree k v -> [Int]))
+blacksToLeaves :: TExpQ (RBTree k v -> [Int])
 blacksToLeaves =
     [||
-         let blacksToLeavesFrom :: Int -> RBTree k v -> [Int]
-             blacksToLeavesFrom n = \case
-                Leaf -> [n+1]
-                Branch R _ l _ _ r -> blacksToLeavesFrom n l ++ blacksToLeavesFrom n r
-                Branch B _ l _ _ r -> blacksToLeavesFrom (n+1) l ++ blacksToLeavesFrom (n+1) r
-         in blacksToLeavesFrom 0
+    let blacksToLeavesFrom :: Int -> RBTree k v -> [Int]
+        blacksToLeavesFrom n = \case
+          Leaf -> [n+1]
+          Branch R _ l _ _ r -> blacksToLeavesFrom n l ++ blacksToLeavesFrom n r
+          Branch B _ l _ _ r -> blacksToLeavesFrom (n+1) l ++ blacksToLeavesFrom (n+1) r
+    in blacksToLeavesFrom 0
     ||]
 
-checkChildren :: Q (TExp (RBTree k v -> Bool))
+checkChildren :: TExpQ (RBTree k v -> Bool)
 checkChildren =
-    [|| \t ->
+    [||
+    \t ->
         let
             checkVsParentColor _ Leaf = True
             -- Red child of red parent - invariant violation!
@@ -166,47 +174,57 @@ checkChildren =
         in checkVsParentColor ($$color t) t
     ||]
 
-keysSorted :: Q (TExp (Comparison k -> RBTree k v -> Bool))
-keysSorted =
-    [|| \comp t ->
-            let sorted [] = True
-                sorted [_] = True
-                sorted (x:y:xys) = comp x y == LT && sorted (y:xys)
-            in sorted $ $$keys t
+sorted :: TExpQ (Comparison k -> [k] -> Bool)
+sorted =
+    [||
+    let sorted :: Comparison k -> [k] -> Bool
+        sorted _ [] = True
+        sorted _ [_] = True
+        sorted comp (x:y:xys) = comp x y == LT && sorted comp (y:xys)
+    in sorted
     ||]
 
-correctBlackHeight :: Q (TExp (RBTree k v -> Bool))
+keysSorted :: TExpQ (Comparison k -> RBTree k v -> Bool)
+keysSorted = [|| \comp t -> $$sorted comp $ $$keys t ||]
+
+correctBlackHeight :: TExpQ (RBTree k v -> Bool)
 correctBlackHeight =
-    [|| \t ->
-            let correct n Leaf = n == 0
-                correct n (Branch R h l _ _ r) = n == h' && correct n l && correct n r
-                  where h' = h - 1
-                correct n (Branch B h l _ _ r) = n == h && correct n' l && correct n' r
-                  where n' = n - 1
-            in correct ($$blackHeight t) t
+    [||
+    let correctBlackHeight :: forall k v . RBTree k v -> Bool
+        correctBlackHeight t = correct ($$blackHeight t) t where
+            correct :: BlackHeight -> RBTree k v -> Bool
+            correct n Leaf = n == 0
+            correct n (Branch R h l _ _ r) = n == h' && correct n l && correct n r
+                where h' = h - 1
+            correct n (Branch B h l _ _ r) = n == h && correct n' l && correct n' r
+                where n' = n - 1
+    in correctBlackHeight
     ||]
 
-valid :: Q (TExp (Comparison k -> RBTree k v -> Bool))
+valid :: TExpQ (Comparison k -> RBTree k v -> Bool)
 valid = [|| \comp t -> $$isBalanced t && $$correctBlackHeight t && $$keysSorted comp t ||]
 
 ------------------------------------------------------------
 -- Colour switching
 ------------------------------------------------------------
 
-redden :: Q (TExp (RBTree k v -> RBTree k v))
+redden :: TExpQ (RBTree k v -> RBTree k v)
 redden = [|| \case { Leaf -> Leaf; Branch _ h l k v r -> Branch R h l k v r } ||]
 
-blacken :: Q (TExp (RBTree k v -> RBTree k v))
+blacken :: TExpQ (RBTree k v -> RBTree k v)
 blacken = [|| \case { Leaf -> Leaf; Branch _ h l k v r -> Branch B h l k v r } ||]
 
 ------------------------------------------------------------
 -- Insertion and balancing
 ------------------------------------------------------------
 
-insert :: Q (TExp (Comparison k -> k -> v -> RBTree k v -> RBTree k v))
+insert :: TExpQ (Comparison k -> k -> v -> RBTree k v -> RBTree k v)
 insert =
-    [|| \comp k v t ->
-        let go = \case
+    [||
+    let insert :: forall k v . Comparison k -> k -> v -> RBTree k v -> RBTree k v
+        insert comp k v t = $$blacken (go t) where
+            go :: RBTree k v -> RBTree k v
+            go = \case
                 Leaf -> Branch R 1 Leaf k v Leaf
                 (Branch B h l k' v' r) -> case comp k k' of
                     LT -> $$balanceL (Branch B h (go l) k' v' r)
@@ -216,7 +234,7 @@ insert =
                     LT -> Branch R h (go l) k' v' r
                     GT -> Branch R h l k' v' (go r)
                     EQ -> Branch R h l k' v r
-        in $$blacken (go t)
+    in insert
     ||]
 
 {- Note [Balancing]
@@ -266,9 +284,11 @@ R2:
 -}
 
 -- | Correct a local imbalance on the left side of the tree.
-balanceL :: Q (TExp (RBTree k v -> RBTree k v))
+balanceL :: TExpQ (RBTree k v -> RBTree k v)
 balanceL =
-    [|| \t -> case t of
+    [||
+    let balanceL :: RBTree k v -> RBTree k v
+        balanceL t =  case t of
             Branch B h toSplit k3 v3 t4 -> case toSplit of
                 -- See note [Balancing], this is case L1
                 Branch R _ (Branch R _ t1 k1 v1 t2) k2 v2 t3 ->
@@ -278,12 +298,15 @@ balanceL =
                     Branch R (h+1) (Branch B h t1 k1 v1 t2) k2 v2 (Branch B h t3 k3 v3 t4)
                 _ -> t
             _ -> t
+    in balanceL
     ||]
 
 -- | Correct a local imbalance on the right side of the tree.
-balanceR :: Q (TExp (RBTree k v -> RBTree k v))
+balanceR :: TExpQ (RBTree k v -> RBTree k v)
 balanceR =
-    [|| \t -> case t of
+    [||
+    let balanceR :: RBTree k v -> RBTree k v
+        balanceR t =  case t of
             Branch B h t1 k1 v1 toSplit -> case toSplit of
                 -- See note [Balancing], this is case R1
                 Branch R _ t2 k2 v2 (Branch R _ t3 k3 v3 t4) ->
@@ -293,6 +316,7 @@ balanceR =
                     Branch R (h+1) (Branch B h t1 k1 v1 t2) k2 v2 (Branch B h t3 k3 v3 t4)
                 _ -> t
             _ -> t
+    in balanceR
     ||]
 
 ------------------------------------------------------------
@@ -302,49 +326,59 @@ balanceR =
 -- | Join two trees, with a key-value mapping in the middle. The keys in the
 -- left tree are assumed to be less than the key in the middle, which is less than the
 -- keys in the right tree.
-join :: Q (TExp (Comparison k -> k -> v -> RBTree k v -> RBTree k v -> RBTree k v))
-join = [|| \comp k v ->
-    let bh = $$blackHeight
-        ins = $$insert comp k v
+join :: TExpQ (Comparison k -> k -> v -> RBTree k v -> RBTree k v -> RBTree k v)
+join =
+    [||
+    let join :: forall k v . Comparison k -> k -> v -> RBTree k v -> RBTree k v -> RBTree k v
+        join comp k v = go where
+            bh = $$blackHeight
+            ins = $$insert comp k v
 
-        join Leaf t2 = ins t2
-        join t1 Leaf = ins t1
-        join t1 t2 = case $$intCompare h1 h2 of
-            LT -> $$blacken $ joinLT t1 t2 h1
-            GT -> $$blacken $ joinGT t1 t2 h2
-            EQ -> $$blacken $ joinEQ t1 t2 h1
-          where
-            h1 = bh t1
-            h2 = bh t2
+            go :: RBTree k v -> RBTree k v -> RBTree k v
+            go Leaf t2 = ins t2
+            go t1 Leaf = ins t1
+            go t1 t2 = case $$(P.intCompare) h1 h2 of
+                LT -> $$blacken $ joinLT t1 t2 h1
+                GT -> $$blacken $ joinGT t1 t2 h2
+                EQ -> $$blacken $ joinEQ t1 t2 h1
+                where
+                    h1 = bh t1
+                    h2 = bh t2
 
-        joinLT t1 t2@(Branch c h l k' v' r) h1
-          | h == h1   = joinEQ t1 t2 h
-          | otherwise = $$balanceL (Branch c h (joinLT t1 l h1) k' v' r)
-        joinLT t1 Leaf _ = t1
+            joinLT :: RBTree k v -> RBTree k v -> BlackHeight -> RBTree k v
+            joinLT t1 t2@(Branch c h l k' v' r) h1
+              | h == h1   = joinEQ t1 t2 h
+              | otherwise = $$balanceL (Branch c h (joinLT t1 l h1) k' v' r)
+            joinLT t1 Leaf _ = t1
 
-        joinGT t1@(Branch c h l k' v' r) t2 h2
-          | h == h2   = joinEQ t1 t2 h
-          | otherwise = $$balanceR (Branch c h l k' v' (joinGT r t2 h2))
-        joinGT Leaf t2 _ = t2
+            joinGT :: RBTree k v -> RBTree k v -> BlackHeight -> RBTree k v
+            joinGT t1@(Branch c h l k' v' r) t2 h2
+              | h == h2   = joinEQ t1 t2 h
+              | otherwise = $$balanceR (Branch c h l k' v' (joinGT r t2 h2))
+            joinGT Leaf t2 _ = t2
 
-        joinEQ t1 t2 h = Branch R (h+1) t1 k v t2
+            joinEQ :: RBTree k v -> RBTree k v -> BlackHeight -> RBTree k v
+            joinEQ t1 t2 h = Branch R (h+1) t1 k v t2
     in join
     ||]
 
 -- | Given a key @k@, spits a tree into a left tree with keys less than @k@, optionally a value
 -- corresponding to the mapping for @k@ if there is one, and a right tree with keys greater
 -- than @k@.
-split :: Q (TExp (Comparison k -> k -> RBTree k v -> (RBTree k v, Maybe v, RBTree k v)))
+split :: TExpQ (Comparison k -> k -> RBTree k v -> (RBTree k v, Maybe v, RBTree k v))
 split =
-    [|| \comp needle ->
-        let jn = $$join comp
+    [||
+    let split :: forall k v . Comparison k -> k -> RBTree k v -> (RBTree k v, Maybe v, RBTree k v)
+        split comp needle = go where
+            jn = $$join comp
+            go :: RBTree k v -> (RBTree k v, Maybe v, RBTree k v)
             go = \case
                 Leaf -> (Leaf, Nothing, Leaf)
                 Branch _ _ l k v r -> case comp needle k of
                     LT -> let (l', mid, r') = go l in (l', mid, jn k v r' ($$blacken r))
                     GT -> let (l', mid, r') = go r in (jn k v ($$blacken l) l', mid, r')
                     EQ -> ($$blacken l, Just v, $$blacken r)
-        in go
+    in split
     ||]
 
 ------------------------------------------------------------
@@ -354,41 +388,47 @@ split =
 -- | Union two trees, keeping both values for a key in case it appears on both sides.
 -- This is the most general union function, but it is less efficient than 'unionWith'
 -- and 'union'.
-unionThese :: Q (TExp (Comparison k -> RBTree k a -> RBTree k b -> RBTree k (These a b)))
+unionThese :: TExpQ (Comparison k -> RBTree k a -> RBTree k b -> RBTree k (These a b))
 unionThese =
-    [|| \comp ->
-        -- The cases for leaves meant that this is less efficient than a simple union:
-        -- it must look at every element (this is obvious from the type!).
-        let union l Leaf = $$blacken $ $$map This l
-            union Leaf r = $$blacken $ $$map That r
-            union (Branch _ _ l k v r) t =
+    [||
+    let union :: forall k a b. Comparison k -> RBTree k a -> RBTree k b -> RBTree k (These a b)
+        union comp = go where
+            -- The cases for leaves meant that this is less efficient than a simple union:
+            -- it must look at every element (this is obvious from the type!).
+            go :: RBTree k a -> RBTree k b -> RBTree k (These a b)
+            go l Leaf = $$blacken $ $$map This l
+            go Leaf r = $$blacken $ $$map That r
+            go (Branch _ _ l k v r) t =
                 -- There are several ways to do a union of RBTree. This way has the
                 -- key advantage of pulling out the mapping for k if there is one,
                 -- so we can package it up in a 'These' properly.
                 let (l', maybeV, r') = $$split comp k t
                     theseVs = $$andMaybe v maybeV
-                in $$join comp k theseVs (union l l') (union r r')
-        in union
+                in $$join comp k theseVs (go l l') (go r r')
+    in union
     ||]
 
 -- | Union two trees, using the given function to compute a value when a key has a
 -- mapping on both sides.
-unionWith :: Q (TExp (Comparison k -> (a -> a -> a) -> RBTree k a -> RBTree k a -> RBTree k a))
+unionWith :: TExpQ (Comparison k -> (a -> a -> a) -> RBTree k a -> RBTree k a -> RBTree k a)
 unionWith =
-    [|| \comp with->
-        let union l Leaf = $$blacken $ l
-            union Leaf r = $$blacken $ r
-            union (Branch _ _ l k v r) t =
+    [||
+    let unionWith :: forall k a . Comparison k -> (a -> a -> a) -> RBTree k a -> RBTree k a -> RBTree k a
+        unionWith comp with = go where
+            go :: RBTree k a -> RBTree k a -> RBTree k a
+            go l Leaf = $$blacken $ l
+            go Leaf r = $$blacken $ r
+            go (Branch _ _ l k v r) t =
                 let (l', maybeV, r') = $$split comp k t
                     newV = case maybeV of
                         Just v' -> with v v'
                         Nothing -> v
-                in $$join comp k newV (union l l') (union r r')
-        in union
+                in $$join comp k newV (go l l') (go r r')
+     in unionWith
     ||]
 
 -- | Left-biased union of two trees.
-union :: Q (TExp (Comparison k -> RBTree k a -> RBTree k a -> RBTree k a))
+union :: TExpQ (Comparison k -> RBTree k a -> RBTree k a -> RBTree k a)
 union = [|| \comp -> $$(unionWith) comp (\a _ -> a) ||]
 
 ------------------------------------------------------------
