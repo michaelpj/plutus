ba69808fa3c16cfc27ccbbb904c1acab37214342
Michael Peyton Jones <michael.peyton-jones@iohk.io>
2019-06-10 12:31:34 +0100

    plutus-ir: non-strict lets

--- plutus-ir/test/TransformSpec.hs
+++ plutus-ir/test/TransformSpec.hs
@@ -7,16 +7,24 @@ import           TestLib
 import           Language.PlutusCore.Quote
 
 import           Language.PlutusIR.Parser
+import           Language.PlutusIR.Transform.NonStrict
 import           Language.PlutusIR.Transform.ThunkRecursions
 
 transform :: TestNested
 transform = testNested "transform" [
     thunkRecursions
+    , nonStrict
     ]
 
 thunkRecursions :: TestNested
 thunkRecursions = testNested "thunkRecursions"
-    $ map (goldenPir (runQuote . thunkRecursionsTerm) term)
+    $ map (goldenPir thunkRecursions term)
     [ "listFold"
     , "monoMap"
+    ]
+
+nonStrict :: TestNested
+nonStrict = testNested "nonStrict"
+    $ map (goldenPir (runQuote . compileNonStrictBindings) term)
+    [ "nonStrict1"
     ]
