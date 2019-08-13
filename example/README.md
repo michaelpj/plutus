070619d4ef7eeb4ad0b2cb73c8054ec9a4c11ac0
David Smith <shmish111+github@gmail.com>
2019-08-13 19:00:14 +0100

    An example project that uses plutus as a library

---
+++ example/README.md
@@ -0,0 +1,47 @@
+# Plutus example project
+
+## NixOS
+
+This directory also includes a `configuration.nix` file which can be used in a NixOS installation. This makes sure the correct system libraries are available as well as adding the Haskell IDE Engine cachix nix cache. This `configuration.nix` file can be used on a clean NixOS installation and everything will just work.
+
+You can use this to easily setup a Virtual Box VM:
+
+1. download the VirtualBox appliance from https://nixos.org/nixos/download.html
+2. import the appliance
+3. before starting the VM make sure you give it plenty of memory, 6Gb should be plenty
+4. start the machine
+5. run `sudo -i` with password `demo`
+6. replace `/etc/nixos/configuration.nix` with the file in this directory
+7. run `nixos-rebuild switch`
+8. run `exit` to get back to the demo user shell
+
+## Getting started
+
+```bash
+# The plutus project contains this example project
+git clone https://github.com/input-output-hk/plutus.git
+
+# copy the project for yourself
+cp -r plutus/example my-example
+cd my-example
+
+# We use a nix shell to ensure all tools and paths are set up correctly
+nix-shell
+
+# update cabal for the first time
+cabal new-update
+
+# tell cabal where the system libraries are, this is needed for the openssl libs
+# if you are not using NixOS then you should change this to the location of the openssl libs
+cabal user-config update -a "extra-lib-dirs: /run/current-system/sw/lib"
+
+# build the project
+cabal new-build
+
+# install the haskell extension for vs code
+code --install-extension alanz.vscode-hie-server
+
+# launch vs code from this nix-shell
+# You must run `code` from within the nix shell in order for Haskell IDE Engine to work correctly
+code
+```
---
+++ example/configuration.nix
@@ -0,0 +1,63 @@
+{ config, pkgs, ... }:
+let
+  unstableTarball =
+    fetchTarball
+      https://github.com/NixOS/nixpkgs-channels/archive/nixos-unstable.tar.gz;
+in
+{
+  imports = [ <nixpkgs/nixos/modules/installer/virtualbox-demo.nix> ];
+
+  nix = {
+    binaryCaches = [
+      "https://cache.nixos.org/"
+      "https://all-hies.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "all-hies.cachix.org-1:JjrzAOEUsD9ZMt8fdFbzo3jNAyEWlPAwdVuHw4RD43k="
+    ];
+  }; 
+
+  nixpkgs.config = {
+    packageOverrides = pkgs: {
+      unstable = import unstableTarball {
+        config = config.nixpkgs.config;
+      };
+    };
+  };
+
+  nixpkgs.config.allowUnfree = true;
+
+  # Let demo build as a trusted user.
+# nix.trustedUsers = [ "demo" ];
+
+# Mount a VirtualBox shared folder.
+# This is configurable in the VirtualBox menu at
+# Machine / Settings / Shared Folders.
+# fileSystems."/mnt" = {
+#   fsType = "vboxsf";
+#   device = "nameofdevicetomount";
+#   options = [ "rw" ];
+# };
+
+# By default, the NixOS VirtualBox demo image includes SDDM and Plasma.
+# If you prefer another desktop manager or display manager, you may want
+# to disable the default.
+# services.xserver.desktopManager.plasma5.enable = lib.mkForce false;
+# services.xserver.displayManager.sddm.enable = lib.mkForce false;
+
+# Enable GDM/GNOME by uncommenting above two lines and two lines below.
+# services.xserver.displayManager.gdm.enable = true;
+# services.xserver.desktopManager.gnome3.enable = true;
+
+# Set your time zone.
+# time.timeZone = "Europe/Amsterdam";
+
+# List packages installed in system profile. To search, run:
+# \$ nix search wget
+ environment.systemPackages = with pkgs; [
+   wget vim openssl openssl.dev openssl.out git
+ ];
+
+# Enable the OpenSSH daemon.
+ services.openssh.enable = true;
+}
